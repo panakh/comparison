f9d652db5ff93027426019a4fe3124f7b983d8ad
Hash Panak <hashinp@gmail.com>
2019-09-25 21:55:51 +0100

    total plan cost

--- src/Comparison/TotalPlanCost.php
+++ src/Comparison/TotalPlanCost.php
@@ -14,7 +14,7 @@ class TotalPlanCost
      */
     private $billedAmount;
 
-    public function __construct(Energy $billableUsage, Price $billedAmount)
+    private function __construct(Energy $billableUsage, Price $billedAmount)
     {
         $this->billableUsage = $billableUsage;
         $this->billedAmount = $billedAmount;
@@ -28,7 +28,7 @@ class TotalPlanCost
         return $totalPlanCost;
     }
 
-    public function calculateCost(Plan $plan, VAT $vat)
+    private function calculateCost(Plan $plan, VAT $vat)
     {
         /** @var CappedRate $cappedRate */
         foreach ($plan->getCappedRates() as $cappedRate) {
@@ -43,7 +43,7 @@ class TotalPlanCost
         $this->applyVAT($vat);
     }
 
-    public function applyCappedRate(CappedRate $cappedRate): void
+    private function applyCappedRate(CappedRate $cappedRate): void
     {
         if ($this->billableUsage === 0 ) {
             return;
@@ -62,18 +62,18 @@ class TotalPlanCost
         }
     }
 
-    public function hasUnbilledUsage(): bool
+    private function hasUnbilledUsage(): bool
     {
         return $this->billableUsage > 0;
     }
 
-    public function applyRate(Rate $rate): void
+    private function applyRate(Rate $rate): void
     {
         $this->billedAmount = $this->billedAmount->addPrice($rate->getPriceForUsage($this->billableUsage));
         $this->billableUsage = Energy::zero();
     }
 
-    public function applyStandingCharge(?Price $standingCharge): void
+    private function applyStandingCharge(?Price $standingCharge): void
     {
         if (null === $standingCharge) {
             return;
@@ -82,7 +82,7 @@ class TotalPlanCost
         $this->billedAmount = $this->billedAmount->addPrice($standingCharge->times(365));
     }
 
-    public function applyVAT(VAT $vat): void
+    private function applyVAT(VAT $vat): void
     {
         $this->billedAmount = $this->billedAmount->addVAT($vat->getPercentage());
     }
